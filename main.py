ace5612062e2076fff26c3d2d7e70ee0b6996ca7
jeina7 <jane1231001@gmail.com>
2019-07-18 18:11:16 +0900

    [update] save blog urls json file with filtering with reactions

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from slack_export import *
-import os
+import os, json
 
 if __name__ == "__main__":
 
+    # save raw data from slack
+    # It will generate data files in output Directory
     slack_token = os.getenv('SLACK_TOKEN')
 
     parser = argparse.ArgumentParser(description='Export Slack history')
@@ -99,3 +101,49 @@ if __name__ == "__main__":
     print('\noutputs Directory: %s' % outputDirectory)
 
     finalize(zipName)
+
+
+    # Get user data
+    users = {}
+    with open(os.path.join(outputDirectory, 'users.json')) as json_file:
+        user_json = json.load(json_file)
+        for user in user_json:
+            if not (user['is_bot'] or (user['name'] == 'slackbot')):
+                if 'real_name' in user.keys():
+                    users[user['id']] = user['real_name']
+                else:
+                    users[user['id']] = user['profile']['real_name']
+
+    # Get Filtered URLs from saved data
+    # need dataz only from [3_*] channels
+    all_channels = os.listdir(outputDirectory)
+    filtered_channels = sorted([i for i in all_channels if i.startswith('3_')])
+
+    # get all data from filtered channels
+    all_messages = []
+    for channel in filtered_channels:
+        channel_path = os.path.join(outputDirectory, channel)
+        for date in sorted(os.listdir(channel_path)):
+            with open(os.path.join(channel_path, date)) as json_file:
+                json_data = json.load(json_file)
+                all_messages.extend(json_data)
+
+    # filter data with reactions
+    # '+1' reaction으로 filtering
+    filter_reaction = '+1'
+    urlz = []
+    for message in all_messages:
+        if ('attachments' in message.keys()) and ('reactions' in message.keys()):
+            reactions = [reaction['name'] for reaction in message['reactions']]
+            if filter_reaction in reactions:
+                userId = message['user']
+                title = message['attachments'][0]['title']
+                link = message['attachments'][0]['title_link']
+                time = str(datetime.fromtimestamp(float(message['ts'])))[:-7]
+
+                url_data = {'userId': userId, 'userName': users[userId], 'title': title, 'url': link, 'time': time}
+                urlz.append(url_data)
+
+    # save as json file
+    with open('urlz.json', 'w') as json_file:
+        json.dump(urlz, json_file, indent=4)
