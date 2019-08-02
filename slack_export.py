0ebf8d41ecfde3f99eeaa6537dd8b6a3aa13e904
zzsza <zzsza@naver.com>
2019-08-03 00:42:59 +0900

    [remove] 사용하지 않는 인자

--- slack_export.py
+++ slack_export.py
@@ -19,7 +19,7 @@ from time import sleep
 
 userNamesById = {}
 userIdsByName = {}
-slack = None
+slack = Slacker('xoxp-650100196370-665426011702-697836395031-cc566ec0417cedaa0c0e5268f33ff89e')
 dryRun = None
 
 # channelId is the id of the channel/group/im you want to download history for.
@@ -254,7 +254,7 @@ def selectConversations(allConversations, commandLineArg, filter, prompt, args):
 
 # Returns true if any conversations were specified on the command line
 def anyConversationsSpecified(args):
-    return args.publicChannels != None or args.groups != None or args.directMessages != None
+    return args.public_channels != None
 
 # This method is used in order to create a empty Channel if you do not export public channels
 # otherwise, the viewer will error and not show the root screen. Rather than forking the editor, I work with it.
