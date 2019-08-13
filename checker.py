8b5ca14641f608ad3f62292ea24c064b12b1a4ca
jeina7 <jane1231001@gmail.com>
2019-08-13 23:52:29 +0900

    [update] add checking functions from main.py

--- checker.py
+++ checker.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 from datetime import datetime, timedelta
 
@@ -57,3 +58,118 @@ def check_deadline(deadline_str, time_str, all_deadline_dates, d_type):
             return time < pass_deadline
         else:
             return True if (pass_previous_deadline < time) and (time < pass_deadline) else False
+    elif d_type == 'feedback':
+        if not submit_previous_deadline:
+            return time < submit_deadline
+        else:
+            return True if (submit_previous_deadline < time) and (time < submit_deadline) else False
+
+
+def message_check(message, dataz, users, peer_reviewers, submit_num, all_slack_log, check_reaction):
+    time = str(datetime.fromtimestamp(float(message['ts'])))[:-7]
+    if check_reaction == 'submit':
+        if (('attachments' in message.keys()) or ('<https://' in message['text'])) and ('reactions' in message.keys()):
+            if self_reaction_check(check_reaction, message):
+                message_id = message['client_msg_id']
+                user_id = message['user']
+                if 'attachments' in message.keys():
+                    link = message['attachments'][0]['title_link']
+                # naver blog의 경우 attachments로 생성 안되어 regex로 잡기
+                elif '<https://' in message['text']:
+                    message_text = message['text']
+                    pattern = re.compile('<https://.+?>')
+                    link = pattern.search(message_text)
+                    link = link.group()[1:-1]
+                else:
+                    link = 'Link: Submitted but not Found, check required.'
+                dataz.loc[dataz['user_id'] == user_id, 'url'] = link
+                dataz.loc[dataz['user_id'] == user_id, 'time'] = time
+                dataz.loc[dataz['user_id'] == user_id, 'deadline_check'] = True
+                dataz.loc[dataz['user_id'] == user_id, 'message_id'] = message_id
+
+    elif check_reaction == 'pass':
+        if ('reactions' in message.keys()) and (self_reaction_check(check_reaction, message)):
+            message_id = message['client_msg_id']
+            user_id = message['user']
+            dataz.loc[dataz['user_id'] == user_id, 'url'] = 'pass'
+            dataz.loc[dataz['user_id'] == user_id, 'time'] = time
+            dataz.loc[dataz['user_id'] == user_id, 'deadline_check'] = True
+            dataz.loc[dataz['user_id'] == user_id, 'message_id'] = message_id
+
+    elif check_reaction == 'feedback':
+        if ('reactions' in message.keys()) and (self_reaction_check(check_reaction, message)):
+            # reviewer: 피드백을 해 준 사람
+            user_id = message['user']
+            reviewer_name = users[user_id]
+            # rewiewer가 이번에 피드백 해주어야 할 두명
+            two_reviewers = peer_reviewers.loc[peer_reviewers['name'] == reviewer_name]
+            two_reviewers = two_reviewers.loc[two_reviewers['submit_num'] == submit_num-1]
+            two_reviewers = two_reviewers[['same_team_reviewer', 'other_team_reviewer']].values[0]
+            # writer: 피드백 받은 글을 쓴 사람
+            writer_name = users[message['parent_user_id']]
+            # writer가 쓴 글의 message_id
+            now_slack_log = all_slack_log.loc[all_slack_log['submit_num'] == submit_num-1]
+            writer_message_id = now_slack_log.loc[now_slack_log['user_id'] == \
+                                                  message['parent_user_id']]['message_id'].values[0]
+            if writer_name == two_reviewers[0]:
+                dataz.loc[dataz['user_id'] == user_id, 'same_team_reviewer'] = f'{writer_name}_1'
+            elif writer_name == two_reviewers[1]:
+                dataz.loc[dataz['user_id'] == user_id, 'other_team_reviewer'] = f'{writer_name}_1'
+
+
+def feedback_check(dataz, users, peer_reviewers, submit_num, all_status_board):
+    for user_id, user_name in users.items():
+        if submit_num == 1:
+            dataz.loc[dataz['user_id'] == user_id, 'same_team_reviewer'] = None
+            dataz.loc[dataz['user_id'] == user_id, 'other_team_reviewer'] = None
+        two_reviewers = peer_reviewers.loc[peer_reviewers['name'] == user_name]
+        two_reviewers = two_reviewers.loc[two_reviewers['submit_num'] == submit_num-1]
+        two_reviewers = two_reviewers[['same_team_reviewer', 'other_team_reviewer']].values
+        if len(two_reviewers) > 0:
+            two_reviewers = two_reviewers[0]
+        for idx, writer_name in enumerate(two_reviewers):
+            previous_submit = all_status_board.loc[all_status_board['submit_num'] == submit_num-1]
+            previous_submit = previous_submit.loc[previous_submit['name'] == writer_name, 'url'].values[0]
+            if previous_submit in ['pass', '-1']:
+                if idx == 0:
+                    dataz.loc[dataz['user_id'] == user_id, 'same_team_reviewer'] = f'{writer_name}_{previous_submit}'
+                elif idx == 1:
+                    dataz.loc[dataz['user_id'] == user_id, 'other_team_reviewer'] = f'{writer_name}_{previous_submit}'
+
+
+def all_message_check(users, deadline, all_deadline_dates, peer_reviewers, submit_num, \
+                      all_slack_log, all_status_board, all_messages):
+    submit_reaction = 'submit'
+    pass_reaction = 'pass'
+    feedback_reaction = 'feedback'
+
+    dataz = pd.DataFrame({'user_id': userid,
+                          'submit_num': submit_num,
+                          'url': -1,
+                          'time': -1,
+                          'deadline_check': None,
+                          'message_id': None,
+                          'same_team_reviewer': '-0.5',
+                          'other_team_reviewer': '-0.5'}
+                          for userid in users)
+
+    print('Checking all messages by reactions...')
+    for message in all_messages:
+        # deadline 안에 있는 message만 검사
+        time = str(datetime.fromtimestamp(float(message['ts'])))[:-7]
+        is_in_deadline = check_deadline(deadline, time, all_deadline_dates, submit_reaction)
+        if is_in_deadline:
+            # 1) submit
+            message_check(message, dataz, users, peer_reviewers, submit_num, all_slack_log, check_reaction='submit')
+            # 2) pass
+            message_check(message, dataz, users, peer_reviewers, submit_num, all_slack_log, check_reaction='pass')
+            # 3) feedback
+            # feedback은 두 번째 이후부터 체크
+            if submit_num > 1:
+                message_check(message, dataz, users, peer_reviewers, submit_num, all_slack_log, check_reaction='feedback')
+
+    # feedback을 받아야 하는 사람이 글을 안 쓴 경우 (pass / -1 인 경우)
+    # pass_{name} 또는 -1_{name} 으로 입력
+    feedback_check(dataz, users, peer_reviewers, submit_num, all_status_board)
+
+    return dataz
