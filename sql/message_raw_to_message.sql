bf5eabf185f0f2935b1823da350177ff8c46ffe4
Max Lee <max@vcnc.co.kr>
2020-04-25 18:58:10 +0900

    feedback 의 경우 ts 가 아니라 thread_ts + 2주를 기준으로 마감 시각 결정

--- sql/message_raw_to_message.sql
+++ sql/message_raw_to_message.sql
@@ -21,15 +21,28 @@ from
       from (
         select
           channel_id, ts, insert_ts, due_ts,
+          -- due_ts 이후 메시지 중 가장 앞의 것을 취하고, due_ts 이후 메시지가 없다면 due_ts 이전 메시지 중 가장 뒤의 것을 취합니다.
           if(insert_ts >= due_ts, insert_ts, timestamp_add(current_timestamp(), interval timestamp_diff(due_ts, insert_ts, second) second)) as sorting_ts
         from (
           select
-            channel_id,
-            timestamp_micros(cast(cast(ts as numeric) * 1000000 as int64)) as ts,
-            timestamp_micros(time_ms) as insert_ts,
-            geultto_udf.find_due_ts(timestamp_micros(cast(cast(ts as numeric) * 1000000 as int64))) as due_ts
-          from
-            geultto_4th_prod.message_raw
+            channel_id, ts, insert_ts,
+            -- feedback 은 thread_ts + 2주를 기준으로 마감 시각을 결정합니다. feedback thread 달고 -> 리뷰어 지정되고 -> feedback emoji 만 다는 케이스를 고려하기 위함입니다.
+            -- submit, feedback emoji 를 실수로 함께 매단 케이스가 1건 존재하여 case when 절에서 우선 submit or pass 여부로 분기합니다.
+            case
+              when (select countif(r.name in ('submit', 'pass')) from unnest(reactions) as r) > 0 then geultto_udf.find_due_ts(ts)
+              when (select countif(r.name = 'feedback') from unnest(reactions) as r) > 0 then geultto_udf.find_due_ts(timestamp_add(thread_ts, interval 14 day))
+              else geultto_udf.find_due_ts(ts)
+            end as due_ts
+          from (
+            select
+              channel_id,
+              timestamp_micros(cast(cast(ts as numeric) * 1000000 as int64)) as ts,
+              timestamp_micros(cast(cast(thread_ts as numeric) * 1000000 as int64)) as thread_ts,
+              timestamp_micros(time_ms) as insert_ts,
+              array(select r from unnest(geultto_udf.parse_reactions(replace(reactions, '\'', '\"'))) as r where r.name in ('submit', 'feedback', 'pass')) as reactions
+            from
+              geultto_4th_prod.message_raw
+          )
         )
       )
     )
